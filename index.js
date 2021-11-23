b28025e142033c6d120325a78045cd9400bdce56
Tian Xu <xueqingxu0302@gmail.com>
2021-11-23 10:42:08 -0700

    commit tree e6ebcc866df02225731f80be467ca1eb409bf230

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
+saturdayFun("bathe my dog")
 
 let mondayWork = function(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
 
-let wrapAdjective = function(style="*") {
+let wrapAdjective = function(style) {
   return function(adjective="special") {
     return `You are ${style}${adjective}${style}!`
   }
